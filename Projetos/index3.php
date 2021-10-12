5609e958d96898f8a78d1aae9332dd8f2f4ff1d8
Danprocha1988 <danprocha1988@gmail.com>
2021-10-11 21:30:51 -0300

    bootcamp

---
+++ Projetos/index3.php
@@ -0,0 +1,118 @@
+<html>
+
+	<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Accordion - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Bolo de Chocolate</h3>
+  <div>
+    <p>Massa:</p>
+	
+		<li>4 ovos</li>
+		<li>4 colheres (sopa) de chocolate em pó</li> 
+		<li>2 colheres (sopa) de manteiga</li>
+		<li>3 xícaras (chá) de farinha de trigo</li>
+		<li>2 xícaras (chá) de açúcar</li>
+		<li>2 colheres (sopa) de fermento</li>
+		<li>1 xícara (chá) de leite</li>
+	
+	<p>Calda:</p>
+		<li>2 colheres (sopa) de manteiga</li>
+		<li>7 colheres (sopa) de chocolate em pó</li>
+		<li>2 latas de creme de leite com soro</li> 
+		<li>3 colheres (sopa) de açúcar</li>
+	<p>Modo de Preparo</p>
+	<p>Massa</p>
+		<li>- Em um liquidificador adicione os ovos, o chocolate em pó, a manteiga, a farinha de trigo, o açúcar e o leite, depois bata por 5 minutos.</li>
+		<li>- Adicione o fermento e misture com uma espátula delicadamente</li>
+		<li>- Em uma forma untada, despeje a massa e asse em forno médio (180 ºC) preaquecido por cerca de 40 minutos.</li>
+		<li>- Não se esqueça de usar uma forma alta para essa receita: como leva duas colheres de fermento, ela cresce bastante! Outra solução pode ser colocar apenas uma colher de fermento e manter a sua receita em uma forma pequena.</li>
+	<p>Calda:</p>
+		<li>- Em uma panela, aqueça a manteiga e misture o chocolate em pó até que esteja homogêneo.</li>
+		<li>- Acrescente o creme de leite e misture bem até obter uma consistência cremosa.</li>
+		<li>- Desligue o fogo e acrescente o açúcar.</li>
+		
+  </div>
+  <h3>Bolo de Chocolate com Cenoura</h3>
+  <div>
+    <p>Massa:</p>
+	
+		<li>3 ovos</li>
+		<li>1 xícara de óleo de soja</li> 
+		<li>2 cenouras bem grandes ou 3 se forem médias</li>
+		<li>2 xícaras de farinha de trigo</li>
+		<li>2 xícaras de açúcar</li>
+		<li>1 colher (sopa) de fermento em pó.</li>
+			
+	<p>Calda:</p>
+		<li>4 colheres cheias de açúcar</li>
+		<li>2 colheres cheias de chocolate em pó</li>
+		<li>1 colher de manteiga</li> 
+		<li>Um pouquinho de leite, apenas para desmanchar</li>
+		
+	<p>Modo de Preparo</p>
+	<p>Massa</p>
+		<li>- Bata a cenoura, os ovos e o óleo no liquidificador.</li>
+		<li>- Em uma vasilha à parte, misture os outros ingredientes e coloque em tabuleiro untado.</li>
+		<li>- Asse em forno médio, pré-aquecido, por 30 a 40 minutos.</li>
+		
+	<p>Calda:</p>
+		<li>- Coloque todos os ingredientes em uma panela e leve ao fogo, sempre mexendo, até ferver um pouco.</li>
+		<li>- Faça furos no bolo para o chocolate escorrer pelo meio do bolo.</li>
+	
+  </div>
+  <h3>Section 3</h3>
+  <div>
+    <p>Massa:</p>
+	
+		<li>4 ovos</li>
+		<li>4 colheres (sopa) de chocolate em pó</li> 
+		<li>2 colheres (sopa) de manteiga</li>
+		<li>3 xícaras (chá) de farinha de trigo</li>
+		<li>2 xícaras (chá) de açúcar</li>
+		<li>2 colheres (sopa) de fermento</li>
+		<li>1 xícara (chá) de leite</li>
+	
+	<p>Calda:</p>
+		<li>2 colheres (sopa) de manteiga</li>
+		<li>7 colheres (sopa) de chocolate em pó</li>
+		<li>2 latas de creme de leite com soro</li> 
+		<li>3 colheres (sopa) de açúcar</li>
+  </div> 
+  <h3>Section 4</h3>
+  <div>
+     <p>Massa:</p>
+	
+		<li>4 ovos</li>
+		<li>4 colheres (sopa) de chocolate em pó</li> 
+		<li>2 colheres (sopa) de manteiga</li>
+		<li>3 xícaras (chá) de farinha de trigo</li>
+		<li>2 xícaras (chá) de açúcar</li>
+		<li>2 colheres (sopa) de fermento</li>
+		<li>1 xícara (chá) de leite</li>
+	
+	<p>Calda:</p>
+		<li>2 colheres (sopa) de manteiga</li>
+		<li>7 colheres (sopa) de chocolate em pó</li>
+		<li>2 latas de creme de leite com soro</li> 
+		<li>3 colheres (sopa) de açúcar</li>
+  </div>
+</div>
+ 
+ 
+</body>
+	
+</html>
